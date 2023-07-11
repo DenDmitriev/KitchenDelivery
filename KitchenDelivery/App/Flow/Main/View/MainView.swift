2e367020fb42746b6b03d103ee0fe34633365107
DenDmitriev <dv.denstr@gmail.com>
2023-07-11 16:31:36 +0300

    add structure MVVM

---
+++ KitchenDelivery/App/Flow/Main/View/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  KitchenDelivery
+//
+//  Created by Denis Dmitriev on 11.07.2023.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Main")
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
