8940764ad98775c6d7122abe89a96291dbecb499
DenDmitriev <dv.denstr@gmail.com>
2023-07-11 15:40:48 +0300

    init project

---
+++ KitchenDelivery/KitchenDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenDeliveryApp.swift
+//  KitchenDelivery
+//
+//  Created by Denis Dmitriev on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct KitchenDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
